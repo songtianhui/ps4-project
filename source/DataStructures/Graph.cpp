58f44f4d08dae67035c91f47fbf4f5bd0b8b114d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-05 20:28:47 +0800

    compile

--- source/DataStructures/Graph.cpp
+++ source/DataStructures/Graph.cpp
@@ -95,7 +95,7 @@ std::vector<Edge> Graph::GetEdges() const {
     for (int j=0; j<MAXV; ++j) {
       if(map[i][j]) {
         alledges.push_back(Edge(i, j));
-        printf("Have edge (%d, %d). ", i,j);
+        printf("Have edge (%d, %d)\n", i,j);
       }
     }
   }
