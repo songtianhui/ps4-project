77d0d354f4c83f3e50c109c86631bd7cd4f4f63e
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-05 18:54:20 +0800

    compile

--- include/DataStructures/Edge.h
+++ include/DataStructures/Edge.h
@@ -13,20 +13,4 @@ class Edge {
   int GetDestination() const;
 };
 
-Edge::Edge(int source, int destination){
-  source = source;
-  destination = destination;
-}
-
-Edge::~Edge(){
-}
-
-int Edge::GetSource() const {
-  return source;
-}
-
-int Edge::GetDestination() const {
-  return destination;
-}
-
 #endif
