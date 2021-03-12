cf9a278e9b9796599a8121b7080fd8fbc27ce3da
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-12 08:21:23 -0500

    compile

---
+++ tests/02-test.cpp
@@ -0,0 +1,24 @@
+//
+// Created by sth on 3/12/21.
+//
+
+#include <cassert>
+#include <cstdio>
+#include <DataStructures/WeightedGraph.h>
+
+using namespace std;
+
+WeightedGraph g;
+
+int main() {
+    g.AddVertex(1);
+    g.AddVertex(1000);
+    g.AddVertex(99999);
+    g.AddEdge(1, 1000, 114514);
+    g.AddEdge(1000, 99999, 250);
+    assert(g.AddEdge(1,2, 12) == false);
+    printf("w(1,1000) = %d\n", g.GetWeight(1,1000));
+    printf("w(1000,99999) = %d\n", g.GetWeight(1000,99999));
+    printf("OK!\n");
+    return 0;
+}
