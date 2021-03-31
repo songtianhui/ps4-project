df54d2928fa6f579f1e46b32172055644e6f650e
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-03-31 11:07:05 -0400

    compile

---
+++ tests/04-test.cpp
@@ -0,0 +1,34 @@
+//
+// Created by sth on 3/31/21.
+//
+
+#include <DataStructures/UndirectedWeightedGraph.h>
+#include <DataStructures/UndirectedGraph.h>
+
+
+void printvector(std::vector<WeightedEdge> *wes) {
+    for (auto &e : *wes) {
+        printf("w(%d, %d) = %d\n", e.GetSource(), e.GetDestination(), e.GetWeight());
+    }
+}
+
+void printw(int s, int d) {
+    printf("w(%d, %d) = %d\n", s, d ,g.GetWeight(s,d));
+}
+
+UndirectedWeightedGraph g;
+
+int main() {
+    assert(g.AddVertex(1) == true);
+    assert(g.AddVertex(1000) == true);
+    assert(g.AddVertex(99999) == true);
+    assert(g.AddVertex(0) == true);
+    assert(g.AddVertex(-114514) == true);
+    assert(g.AddVertex(99999) == false);
+    assert(g.AddVertex(0) == false);
+    assert(g.RemoveVertex(1) == true);
+    assert(g.AddVertex(1) == true);
+
+    printf("OK!\n");
+
+}
