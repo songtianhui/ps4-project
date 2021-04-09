e744262d254812d5333827d144193ed96aa0a0e1
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 09:07:00 -0400

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -3,14 +3,55 @@
 
 #include <functional>
 #include <optional>
+#include <unordered_set>
+#include <vector>
+#include <queue>
 
 template<typename TGraph>
 class BreadthFirstSearcher {
 public:
-    static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action);
+    static void VisitAllVertices(const TGraph *graph, int start, const std::function<void(int)> &action) {
+        if (!graph->ContainsVertex(start)) return;
+
+        std::unordered_set<int> vis;
+        std::queue<int> q;
+
+        action(start);
+        q.emplace(start);
+        while (!q.empty()) {
+            int cur = q.front(); q.pop();
+            vis.emplace(cur);
+            for (int next : graph->GetNeighbors(cur)) {
+                if (vis.find(next) == vis.end()) {
+                    action(next);
+                    q.emplace(next);
+                }
+            }
+        }
+    }
 
     static std::optional<int>
-    FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate);
+    FindFirstVertex(const TGraph *graph, int start, const std::function<bool(int)> &predicate) {
+        if (!graph->ContainsVertex(start)) return std::nullopt;
+
+        std::unordered_set<int> vis;
+        std::queue<int> q;
+
+        if (predicate(start)) return start;
+        q.emplace(start);
+        while (!q.empty()) {
+            int cur = q.front(); q.pop();
+            vis.emplace(cur);
+            for (int next : graph->GetNeighbors(cur)) {
+                if (vis.find(next) == vis.end()) {
+                    if (predicate(next)) return next;
+                    q.emplace(next);
+                }
+            }
+        }
+
+        return std::nullopt;
+    }
 };
 
 #endif
