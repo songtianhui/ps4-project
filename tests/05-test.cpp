c8c29d15a251d49b95426d73a438659d5596235b
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 09:03:57 -0400

    compile

--- tests/05-test.cpp
+++ tests/05-test.cpp
@@ -4,10 +4,28 @@
 
 #include <Algorithms/BreadthFirstSearcher.h>
 #include <Algorithms/DepthFirstSearcher.h>
-#include <DataStructures/Graph.h>
+#include <DataStructures/WeightedGraph.h>
 
-Graph g;
+WeightedGraph<int> g;
+
+void print(int x) {
+    printf("%d\n", x);
+}
 
 int main() {
+    assert(g.AddVertex(1) == true);
+    assert(g.AddVertex(1000) == true);
+    assert(g.AddVertex(99999) == true);
+    assert(g.AddVertex(0) == true);
+    assert(g.AddVertex(-114514) == true);
 
+    assert(g.AddEdge(1, 1000, 4) == true);
+    assert(g.AddEdge(0, 1000, 100) == true);
+    assert(g.AddEdge(-114514, 0, -100) == true);
+    assert(g.AddEdge(99999, 0, 0) == true);
+    assert(g.AddEdge(1000, 99999, 250) == true);
+
+    BreadthFirstSearcher<WeightedGraph<int> >::VisitAllVertices(&g, 1, &print);
+
+    return 0;
 }
