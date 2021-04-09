38e792be889e896cd8a997d8bceeb7ca1a758ca2
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-09 10:08:25 -0400

    compile

--- tests/05-test.cpp
+++ tests/05-test.cpp
@@ -4,10 +4,11 @@
 
 #include <Algorithms/BreadthFirstSearcher.h>
 #include <Algorithms/DepthFirstSearcher.h>
+#include <DataStructures/Graph.h>
 #include <DataStructures/WeightedGraph.h>
 #include <DataStructures/UndirectedWeightedGraph.h>
 
-UndirectedWeightedGraph<int> g;
+Graph g;
 
 void print(int x) {
     printf("%d\n", x);
@@ -24,18 +25,18 @@ int main() {
     assert(g.AddVertex(0) == true);
     assert(g.AddVertex(-114514) == true);
 
-    assert(g.AddEdge(1, 1000, 4) == true);
-//    assert(g.AddEdge(0, 1000, 100) == true);
-//    assert(g.AddEdge(-114514, 0, -100) == true);
-    assert(g.AddEdge(99999, 0, 0) == true);
-//    assert(g.AddEdge(1000, 99999, 250) == true);
-    assert(g.AddEdge(99999, 1, 99) == true);
-    assert(g.AddEdge(1000, 1000, 10) == true);
-    assert(g.AddEdge(99999, -114514, 2) == true);
-//    assert(g.AddEdge(0, -114514, -100) == true);
+    assert(g.AddEdge(1, 1000) == true);
+    assert(g.AddEdge(0, 1000) == true);
+    assert(g.AddEdge(-114514, 0) == true);
+    assert(g.AddEdge(99999, 0) == true);
+    assert(g.AddEdge(1000, 99999) == true);
+    assert(g.AddEdge(99999, 1) == true);
+    assert(g.AddEdge(1000, 1000) == true);
+    assert(g.AddEdge(99999, -114514) == true);
+    assert(g.AddEdge(0, -114514) == true);
 
-    DepthFirstSearcher<UndirectedWeightedGraph<int> >::VisitAllVertices(&g, 0, &print);
-    printf("first: %d\n", DepthFirstSearcher<UndirectedWeightedGraph<int> >::FindFirstVertex(&g, 0, &cond));
+    DepthFirstSearcher<Graph>::VisitAllVertices(&g, 0, &print);
+    printf("first: %d\n", DepthFirstSearcher<Graph>::FindFirstVertex(&g, 0, &cond));
 
     return 0;
 }
