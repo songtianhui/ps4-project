6054423807579e3628d94a610705f8dbbd4bf08a
Tianyun Zhang <doowzs@qq.com>
2021-04-13 21:11:38 +0800

    add week7 tasks

---
+++ include/Algorithms/ShortestPaths.h
@@ -0,0 +1,18 @@
+#ifndef SHORTEST_PATHS
+#define SHORTEST_PATHS
+
+#include <vector>
+#include <optional>
+
+template <typename TGraph, typename TValue>
+class ShortestPaths {
+ public:
+  ShortestPaths(const TGraph<TValue> *graph, int source);
+  virtual ~ShortestPaths();
+ public:
+  virtual bool HasPathTo(int destination) = 0;
+  virtual std::optional<TValue> GetDistanceTo(int destination) = 0;
+  virtual std::optional<std::vector<int>> GetShortestPathTo(int destination) = 0;
+};
+
+#undef
