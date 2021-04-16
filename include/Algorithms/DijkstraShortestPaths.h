0dcad52bfb4d520c9ddd641b025711d3e4da6015
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-15 23:09:20 -0400

    compile

---
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -0,0 +1,15 @@
+//
+// Created by sth on 2021/4/15.
+//
+
+#ifndef GRAPHLIBRARY_DIJKSTRASHORTESTPATHS_H
+#define GRAPHLIBRARY_DIJKSTRASHORTESTPATHS_H
+
+#include <Algorithms/ShortestPaths.h>
+
+template<template<typename> class TGraph, typename TValue>
+class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue> {
+
+};
+
+#endif //GRAPHLIBRARY_DIJKSTRASHORTESTPATHS_H
--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -4,16 +4,23 @@
 #include <vector>
 #include <optional>
 
-template <template<typename> class TGraph, typename TValue>
+template<template<typename> class TGraph, typename TValue>
 class ShortestPaths {
- public:
-  ShortestPaths() = delete;
-  ShortestPaths(const TGraph<TValue> *graph, int source);
-  virtual ~ShortestPaths();
- public:
-  bool HasPathTo(int destination) const;
-  std::optional<TValue> TryGetDistanceTo(int destination) const;
-  std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const;
+public:
+    ShortestPaths() = delete;
+
+    ShortestPaths(const TGraph<TValue> *graph, int source);
+
+    virtual ~ShortestPaths();
+
+public:
+    bool HasPathTo(int destination) const {
+
+    }
+
+    std::optional<TValue> TryGetDistanceTo(int destination) const;
+
+    std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const;
 };
 
 #endif
