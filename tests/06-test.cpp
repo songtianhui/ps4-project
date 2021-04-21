de4e3e842cba88b02c5f8375bbbf385f8811574d
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-21 08:52:13 -0400

    compile

--- tests/06-test.cpp
+++ tests/06-test.cpp
@@ -33,8 +33,8 @@ void init_g() {
 int main() {
     init_g();
 
-    DijkstraShortestPaths<WeightedGraph, double> ssp(&g, 1);
-    int dst = 1000;
+    DijkstraShortestPaths<WeightedGraph, double> ssp(&g, -114514);
+    int dst = 1;
     if (ssp.HasPathTo(dst)) printf("HasPath to %d: %lf\n", dst, ssp.HasPathTo(dst));
     printf("distance to %d: %lf\n", dst, ssp.TryGetDistanceTo(dst));
     printf("path to %d:\n", dst);
