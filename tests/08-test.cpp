b1f819388883e59336a1bc8e80cd96dae53dbcda
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-25 09:31:34 -0400

    compile

--- tests/08-test.cpp
+++ tests/08-test.cpp
@@ -26,13 +26,13 @@ void init_g() {
     assert(g.AddVertex(1) == true);
 
 
-//    assert(g.AddEdge(1, 1000, 114514) == true);
-//    assert(g.AddEdge(0, 1000, 100) == true);
-//    assert(g.AddEdge(-114514, 0, -100) == true);
-//    assert(g.AddEdge(99999, 0, 0) == true);
-//    assert(g.AddEdge(1000, 99999, 250) == true);
-//    assert(g.AddEdge(99999, 1, 99) == true);
-//    assert(g.AddEdge(1000, 1000, 10) == true);
+    assert(g.AddEdge(1, 1000, 114514) == true);
+    assert(g.AddEdge(0, 1000, 100) == true);
+    assert(g.AddEdge(-114514, 0, -100) == true);
+    assert(g.AddEdge(99999, 0, 0) == true);
+    assert(g.AddEdge(1000, 99999, 250) == true);
+    assert(g.AddEdge(99999, 1, 99) == true);
+    assert(g.AddEdge(1000, 1000, 10) == true);
 }
 
 int main() {
