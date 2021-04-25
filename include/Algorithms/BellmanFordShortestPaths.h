5f146ba5dcf0115c18bb802f4c6b3afbd8ad290b
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-04-25 09:00:13 -0400

    compile

--- include/Algorithms/BellmanFordShortestPaths.h
+++ include/Algorithms/BellmanFordShortestPaths.h
@@ -6,7 +6,6 @@
 #define GRAPHLIBRARY_BELLMANFORDSHORTESTPATHS_H
 
 #include <Algorithms/ShortestPaths.h>
-#include <queue>
 
 template<typename TGraph>
 class BellmanFordShortestPaths : public ShortestPaths<TGraph> {
@@ -16,7 +15,7 @@ private:
     std::map<int, std::optional<int> >   pre;   // src到点最短路径的该点前一个点
 
 private:
-    void Init() {
+    void init() {
         cost[this->GetSource()] = TValue();
     }
 
@@ -24,49 +23,21 @@ public:
     BellmanFordShortestPaths() = delete;
 
     BellmanFordShortestPaths(const TGraph *graph, int source) : ShortestPaths<TGraph>(graph, source) {
-//        if (!graph->ContainsVertex(source)) return;
-//        Init();
-//        std::unordered_set<int> vis;
-//        for (int i = 1; i < graph->CountVertices(); ++i) {
-//            for (auto &e : graph->GetEdges()) {
-//                const int src = e.GetSource();
-//                const int dst = e.GetDestination();
-//                const TValue w = e.GetWeight();
-//                const std::optional<TValue> cur_cost = cost[src];
-//                // 检查cur_cost
-//                if (cur_cost == std::nullopt) continue;
-//                const TValue new_cost = cur_cost.value() + w;
-//                if (cost[dst] == std::nullopt || new_cost < cost[dst].value()) {
-//                    cost[dst] = new_cost;
-//                    pre[dst] = src;
-//                }
-//            }
-//        }
-
         if (!graph->ContainsVertex(source)) return;
-        Init();
+        init();
         std::unordered_set<int> vis;
-        std::priority_queue<std::pair<TValue, int>, std::vector<std::pair<TValue, int> >, std::greater<std::pair<TValue, int>>> pq;
-        int src = this->GetSource();
-        cost[src] = TValue();
-        pre[src] = src;
-        pq.emplace(TValue(), src);
-        while (!pq.empty()) {
-            const auto state = pq.top();
-            pq.pop();
-            const TValue cur_cost = state.first;
-            const int    cur_idx  = state.second;
-            if (vis.find(cur_idx) != vis.end()) continue;
-            vis.emplace(cur_idx);
-            for (const auto &e : graph->GetOutgoingEdges(cur_idx)) {
-                assert(cur_idx == e.GetSource());
-                const int new_idx  = e.GetDestination();
-                assert(graph->ContainsVertex(new_idx));
-                const TValue new_cost = cur_cost + e.GetWeight();
-                if (cost[new_idx] == std::nullopt || new_cost < cost[new_idx]) {
-                    pq.push({new_cost, new_idx});
-                    cost[new_idx] = new_cost;
-                    pre[new_idx]  = cur_idx;
+        for (int i = 1; i < graph->CountVertices(); ++i) {
+            for (auto &e : graph->GetEdges()) {
+                const int src = e.GetSource();
+                const int dst = e.GetDestination();
+                const TValue w = e.GetWeight();
+                const std::optional<TValue> cur_cost = cost[src];
+                // 检查cur_cost
+                if (cur_cost == std::nullopt) continue;
+                const TValue new_cost = cur_cost.value() + w;
+                if (cost[dst] == std::nullopt || new_cost < cost[dst].value()) {
+                    cost[dst] = new_cost;
+                    pre[dst] = src;
                 }
             }
         }
@@ -97,7 +68,7 @@ public:
         while (cur != src){
             ret.push_back(cur);
             auto it = pre.find(cur);
-            assert(it != pre.end() && it->second != std::nullopt);
+//            assert(it != pre.end() && it->second != std::nullopt);
             cur = it->second.value();
         }
         ret.push_back(src);
