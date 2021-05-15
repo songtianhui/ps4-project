c76d7b7d294e25bfb5a7b82b69079ba5bf73c657
tracer-ps4-2021 <tracer-ps4-2021@ics.nju.edu.cn>
2021-05-15 00:07:37 -0400

    compile

--- include/Algorithms/BiparititeGraphMatching.h
+++ include/Algorithms/BiparititeGraphMatching.h
@@ -2,13 +2,47 @@
 #define GRAPHLIBRARY_BIPARTITEGRAPHMATCHING_H
 #include <unordered_set>
 #include <optional>
+#include <unordered_map>
 
 template<typename TGraph>
 class BipariteGraphMatching {
+private:
+    TGraph *graph;
+    std::unordered_set<int> used;
+    std::unordered_map<int, int> belong;
+    int total;
+
+private:
+    bool find(int x) {
+        for (auto &y : graph->GetNeighbors()) {
+            if (used.find(y) == used.end()) {
+                used.insert(y);
+                if (belong.find(y) == belong.end() || find(belong[y])) {
+                    belong[y] = x;
+                    belong[x] = y;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 public:
-    BipariteGraphMatching(const TGraph* g, std::unordered_set<int> left, std::unordered_set<int> right);
-    int SumOfMatches() const;
-    std::optional<int> FindMatchOf(int idx) const;
+    BipariteGraphMatching(const TGraph* g, std::unordered_set<int> left, std::unordered_set<int> right) {
+        graph = g;
+        for (int i : left) {
+            used.clear();
+            if (find(i)) total++;
+        }
+    }
+
+    int SumOfMatches() const { return total; }
+
+    std::optional<int> FindMatchOf(int idx) {
+        auto it = belong.find(idx);
+        if (it == belong.end()) return std::nullopt;
+        if (!graph->ContainsVertex(idx)) return std::nullopt;
+        return it->second;
+    }
 };
 
 #endif //GRAPHLIBRARY_BIPARTITEGRAPHMATCHING_H
---
+++ tests/11-test.cpp
@@ -0,0 +1,48 @@
+//
+// Created by sth on 2021/5/15.
+//
+
+#include <Algorithms/BiparititeGraphMatching.h>
+#include <DataStructures/UndirectedWeightedGraph.h>
+
+UndirectedWeightedGraph<int> g;
+
+void init_g() {
+    assert(g.AddVertex(1) == true);
+    assert(g.AddVertex(1000) == true);
+    assert(g.AddVertex(99999) == true);
+    assert(g.AddVertex(0) == true);
+    assert(g.AddVertex(-114514) == true);
+    assert(g.AddVertex(99999) == false);
+    assert(g.AddVertex(0) == false);
+    assert(g.RemoveVertex(1) == true);
+    assert(g.RemoveVertex(1) == false);
+    assert(g.AddVertex(1) == true);
+
+
+    assert(g.AddEdge(1, 1000, 114514) == true);
+    assert(g.AddEdge(0, 1000, 100) == true);
+    assert(g.AddEdge(-114514, 0, 100) == true);
+    assert(g.AddEdge(99999, 0, 0) == true);
+    assert(g.AddEdge(1000, 99999, 250) == true);
+    assert(g.AddEdge(99999, 1, 99) == true);
+    assert(g.AddEdge(1000, 1000, 10) == true);
+}
+
+int main() {
+    init_g();
+    std::unordered_set<int> left;
+    std::unordered_set<int> right;
+    left.insert(1);
+    left.insert(1000);
+    right.insert(99999);
+    right.insert(0);
+    right.insert(-114514);
+    BipariteGraphMatching<UndirectedWeightedGraph<int>> bm(&g, left, right);
+    printf("ans = %d\n", bm.SumOfMatches());
+    for (int i : g.GetVertices()) {
+        if (bm.FindMatchOf(i) != std::nullopt)
+            printf("%d : %d\n", i, bm.FindMatchOf(i));
+    }
+    return 0;
+}
